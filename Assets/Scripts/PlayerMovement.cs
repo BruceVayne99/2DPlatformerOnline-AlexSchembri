653f1a6868ea65b288810959d97be9b6bda8a0ba
BruceVayne99 <alex.schembri1@studytafensw.edu.au>
2023-11-18 22:21:35 +1100

    Basic Walk Animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,8 @@ public class PlayerMovement : MonoBehaviour
     public float JumpSpeed = 500f;
 
     private Rigidbody2D _rigidbody;
+    private Animator _animator;
+    private SpriteRenderer _sprite;
 
     private bool _isGrounded = false;
 
@@ -19,13 +21,31 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _sprite = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         // Keyboard A/D or Left/Right arrow
         _XInput = Input.GetAxisRaw("Horizontal");
-        _XInput *= MoveSpeed * Time.deltaTime;
+
+        if (_XInput == 0)
+        {
+            _animator.SetBool("isWalking", false);
+        }
+        else
+        {
+            _animator.SetBool("isWalking", true);
+            if (_XInput > 0)
+            {
+                _sprite.flipX = true;
+            }
+            else
+            {
+                _sprite.flipX = false;
+            }
+        }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -36,6 +56,7 @@ public class PlayerMovement : MonoBehaviour
     {
         //_rigidbody.AddForce(new Vector2(_XInput, 0f), ForceMode2D.Impulse);
         _rigidbody.velocity = new Vector2(_XInput * MoveSpeed * Time.fixedDeltaTime, _rigidbody.velocity.y);
+        _XInput *= MoveSpeed * Time.deltaTime;
 
         if (_isJumpPressed && _isGrounded)
         {
